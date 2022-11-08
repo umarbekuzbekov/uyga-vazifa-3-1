a1d81276c3f316435e012fbe3bdd2ce26480a5f9
umarbekuzbekov <umarbekuzbekov1611@gmail.com>
2022-11-08 08:38:22 -0500

    3 oy 1 uyga vazifa

---
+++ main.js
@@ -0,0 +1,139 @@
+console.log('      *');
+console.log('     ***');
+console.log('    *****');
+console.log('   *******');
+console.log('  *********');
+console.log(' ***********');
+console.log('     | |');
+console.log('     | |');
+console.log(' ___________');
+
+
+var ozgaruvchi ='var kalit sozi yordamida ozgaruvchilar kiritildi';
+console.log(ozgaruvchi)
+
+var userName ='Asror';                              
+console.log(userName)
+var userPhone ='635487150658';
+console.log(userPhone);
+var useraddress ='Toshkent';
+console.log(useraddress);
+var usereducation ='Becholar';
+console.log(usereducation);
+var userInterest ='football';
+console.log(userInterest);
+var userEmail ='*******@.com';
+console.log(userEmail);
+var userDream ='Study abroad';
+console.log(userDream);
+var userCars ='Nissan GTR';
+console.log(userCars);
+var userHobby ='gaming';
+console.log(userHobby);
+var mainColor ='red';
+console.log(mainColor);
+var mainFontFace ='San-serif';
+console.log(mainFontFace);
+var mainLineHeight ='18px';
+console.log(mainLineHeight);
+var mainImg ='Cats';
+console.log(mainImg);
+var mainHero ='Tony Jaa';
+console.log(mainHero);
+var secondImg ='Super Cars';
+console.log(secondImg);
+var mainFootballPleayer ='Ronaldu';
+console.log(mainFootballPleayer);
+var secondaryFootballPleayer ='Messi';
+console.log(secondaryFootballPleayer);
+var mainSport ='Football';
+console.log(mainSport);
+var dovon = '20atlik dovon bosib otildi';
+console.log(dovon)
+
+
+let newOzgaruvchi ='let kalit sozi yordamida ozgaruvchilar kiritildi';
+console.log(newOzgaruvchi);
+
+let newUserName ='Asror';                              
+console.log(newUserName)
+let  newUserPhone ='635487150658';
+console.log(newUserPhone);
+let  newUseraddress ='Toshkent';
+console.log(newUseraddress);
+let  newUsereducation ='Becholar';
+console.log(newUsereducation);
+let  newUserInterest ='football';
+console.log(newUserInterest);
+let  newUserEmail ='*******@.com';
+console.log(newUserEmail);
+let  newUserDream ='Study abroad';
+console.log(newUserDream);
+let  newUserCars ='Nissan GTR';
+console.log(newUserCars);
+let newUserHobby ='gaming';
+console.log(newUserHobby);
+let newMainColor ='red';
+console.log(newMainColor);
+let newMainFontFace ='San-serif';
+console.log(newMainFontFace);
+let newMainLineHeight ='18px';
+console.log(newMainLineHeight);
+let newMainImg ='Cats';
+console.log(newMainImg);
+let newMainHero ='Tony Jaa';
+console.log(newMainHero);
+let newSecondImg ='Super Cars';
+console.log(newSecondImg);
+let newMainFootballPleayer ='Ronaldu';
+console.log(newMainFootballPleayer);
+let newSecondaryFootballPleayer ='Messi';
+console.log(newSecondaryFootballPleayer);
+let newMainSport ='Football';
+console.log(newMainSport);
+let newDovon = '20talik dovon bosib otildi';
+console.log(newDovon);
+
+
+
+const oldOzgaruvchi ='const kalit sozi yordamida ozgaruvchilar kiritildi';
+console.log(oldOzgaruvchi);
+
+const oldUserName ='Asror';                              
+console.log(oldUserName)
+const oldUserPhone ='635487150658';
+console.log(oldUserPhone);
+const  oldUseraddress ='Toshkent';
+console.log(oldUseraddress);
+const  oldUsereducation ='Becholar';
+console.log(oldUsereducation);
+const  oldUserInterest ='football';
+console.log(oldUserInterest);
+const oldUserEmail ='*******@.com';
+console.log(oldUserEmail);
+const oldUserDream ='Study abroad';
+console.log(oldUserDream);
+const  oldUserCars ='Nissan GTR';
+console.log(oldUserCars);
+const oldUserHobby ='gaming';
+console.log(oldUserHobby);
+const oldMainColor ='red';
+console.log(oldMainColor);
+const oldMainFontFace ='San-serif';
+console.log(oldMainFontFace);
+const oldMainLineHeight ='18px';
+console.log(oldMainLineHeight);
+const oldMainImg ='Cats';
+console.log(oldMainImg);
+const oldMainHero ='Tony Jaa';
+console.log(oldMainHero);
+const oldSecondImg ='Super Cars';
+console.log(oldSecondImg);
+const oldMainFootballPleayer ='Ronaldu';
+console.log(oldMainFootballPleayer);
+const oldSecondaryFootballPleayer ='Messi';
+console.log(oldSecondaryFootballPleayer);
+const oldMainSport ='Football';
+console.log(oldMainSport);
+const oldDovon = '20talik dovon bosib otildi';
+console.log(oldDovon);
